6c645c2a9f77f31488f65aef972bb9a86a441382
ajimenez5 <ajimenez5@users.noreply.github.com>
2018-02-17 11:42:58 -0600

    I am confused

--- constitution.py
+++ constitution.py
@@ -4,8 +4,20 @@ Created on Feb 3, 2018
 @author: ITAUser
 '''
 
-file="constituton.txt"
+"""file="constituton.txt"
 numberfile=open(file,"r")
 text = numberfile.read()
 words = text.split() 
-print(len(words))
+print(len(words))"""
+
+
+fname="constituton.txt"
+f=open(fname,"r+")
+
+
+for line in f: 
+    words=line.split()
+    num_words = len(words)
+    for word in words: 
+        I += word.count("p")
+        
